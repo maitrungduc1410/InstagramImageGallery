4d3b57a9df1a25fbe41c3cdfdde285f02afbc797
Duc Trung Mai <maitrungduc1410@gmail.com>
2026-03-16 22:22:28 +0800

    Initial Commit

---
+++ InstagramImageGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstagramImageGallery
+//
+//  Created by Duc Trung Mai on 3/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstagramImageGallery/InstagramImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramImageGalleryApp.swift
+//  InstagramImageGallery
+//
+//  Created by Duc Trung Mai on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
